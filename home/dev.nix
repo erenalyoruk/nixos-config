ebd78bd4527264b861f01084d3e08d7242e34a07
Eren Alyörük <27606018+erenalyoruk@users.noreply.github.com>
2025-07-03 18:41:38 +0300

    vulkan and python

--- home/dev.nix
+++ home/dev.nix
@@ -1,13 +1,18 @@
 { pkgs, ... }: {
   home.packages = with pkgs; [
     nodejs_22
-#    llvmPackages_20.clang-tools
     llvmPackages_20.clang-unwrapped
     gnumake
     ninja
     cmake
+    python314
     jq
     yq
     docker-compose
+    vulkan-tools
+    vulkan-helper
+    vulkan-headers
+    vulkan-extension-layer
+    vulkan-validation-layers
   ];
 }
